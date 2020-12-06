6146244ec8af81dda03cf4d200e71f745e2e9b1d
Baptiste Honnay <honnaybaptiste@gmail.com>
2020-12-06 20:33:14 +0100

    implement - options form

--- src/Components/OptionsPage.js
+++ src/Components/OptionsPage.js
@@ -8,15 +8,15 @@ let pageHtml = `
                 <h4>Volumes:</h4>
                 <div class="form-group">
                     <label for="masterVolume">Principal:</label>
-                    <input type="range" id="masterVolume" name="masterVolume" class="form-control-range w-25">
+                    <input type="range" id="masterVolume" name="masterVolume" class="form-control-range w-25" min="0" max="1" step="0.05">
                 </div>
                 <div class="form-group">
                     <label for="bgmVolume">Musique:</label>
-                    <input type="range" id="bgmVolume" name="bgmVolume" class="form-control-range w-25">
+                    <input type="range" id="bgmVolume" name="bgmVolume" class="form-control-range w-25" min="0" max="1" step="0.05">
                 </div>
                 <div class="form-group">
                     <label for="effectVolume">Effets sonores:</label>
-                    <input type="range" id="effectVolume" name="effectVolume" class="form-control-range w-25">
+                    <input type="range" id="effectVolume" name="effectVolume" class="form-control-range w-25" min="0" max="1" step="0.05">
                 </div>
             </div>
             <div class="border mt-5 pl-3 pt-2">
@@ -25,45 +25,51 @@ let pageHtml = `
                     <div class="col-sm-2 pt-1">Bouton 1:</div>
                     <div class="col-sm-8"></div>
                     <div class="col-sm-1">
-                        <button id="btnKey1" type="button" class="btn btn-outline-primary">D</button>
+                        <button id="btnKey1" type="button" class="btn btn-outline-primary" data-toggle="popover" data-content="Appuiez sur une touche" data-trigger="focus"></button>
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-sm-2 pt-1">Bouton 2:</div>
                     <div class="col-sm-8"></div>
                     <div class="col-sm-1">
-                        <button id="btnKey2" type="button" class="btn btn-outline-primary">F</button>
+                        <button id="btnKey2" type="button" class="btn btn-outline-primary" data-toggle="popover" data-content="Appuiez sur une touche" data-trigger="focus"></button>
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-sm-2 pt-1">Bouton 3:</div>
                     <div class="col-sm-8"></div>
                     <div class="col-sm-1">
-                        <button id="btnKey3" type="button" class="btn btn-outline-primary">J</button>
+                        <button id="btnKey3" type="button" class="btn btn-outline-primary" data-toggle="popover" data-content="Appuiez sur une touche" data-trigger="focus"></button>
                     </div>
                 </div>
                 <div class="row my-3">
                     <div class="col-sm-2 pt-1">Bouton 4:</div>
                     <div class="col-sm-8"></div>
                     <div class="col-sm-1">
-                        <button id="btnKey4" type="button" class="btn btn-outline-primary">K</button>
+                        <button id="btnKey4" type="button" class="btn btn-outline-primary" data-toggle="popover" data-content="Appuiez sur une touche" data-trigger="focus"></button>
                     </div>
                 </div>
             </div>
             <button id="submit" type="submit" class="mt-3 btn btn-primary">Enregistrer</button>
+            <button id="submit" class="mt-3 btn btn-secondary">Défaut</button>
         </form>
     </div>
 </div>`;
 
 let page = document.querySelector("#page");
 
+let btnKey1;
+let btnKey2;
+let btnKey3;
+let btnKey4;
+
 let currentPreferences = { //TODO: fetch from /api/users
     keyBinding: {
-      key1: "d",
-      key2: "f",
-      key3: "j",
-      key4: "k",
-    },
+        1: "d",
+        2: "f",
+        3: "j",
+        4: "k",
+      },
     volume: {
       master: 1,
       bgm: 0.20,
@@ -73,10 +79,10 @@ let currentPreferences = { //TODO: fetch from /api/users
 
 const defaultPreferences = {
     keyBinding: {
-        key1: "d",
-        key2: "f",
-        key3: "j",
-        key4: "k",
+        1: "d",
+        2: "f",
+        3: "j",
+        4: "k",
       },
       volume: {
         master: 1,
@@ -88,19 +94,142 @@ const defaultPreferences = {
 const OptionsPage = () => {
     page.innerHTML = pageHtml;
 
-    let rangeMaster = document.querySelector("#btnKey1");
+    btnKey1 = document.querySelector("#btnKey1");
+    btnKey2 = document.querySelector("#btnKey2");
+    btnKey3 = document.querySelector("#btnKey3");
+    btnKey4 = document.querySelector("#btnKey4");
 
-    let btnKey1 = document.querySelector("#btnKey1");
-    btnKey1.innerText = currentPreferences.keyBinding.key1;
+    let rangeMaster = document.querySelector("#masterVolume");
+    rangeMaster.value = currentPreferences.volume.master;
 
-    let btnKey2 = document.querySelector("#btnKey2");
-    btnKey2.innerText = currentPreferences.keyBinding.key2;
+    let rangeBgm = document.querySelector("#bgmVolume");
+    rangeBgm.value = currentPreferences.volume.bgm;
 
-    let btnKey3 = document.querySelector("#btnKey3");
-    btnKey3.innerText = currentPreferences.keyBinding.key3;
+    let rangeEffect = document.querySelector("#effectVolume");
+    rangeEffect.value = currentPreferences.volume.effect;
 
-    let btnKey4 = document.querySelector("#btnKey4");
-    btnKey4.innerText = currentPreferences.keyBinding.key4;
+    //bootstrap JQuery for popovers
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover();
+    });
+
+    refreshKeyBindingBtn();
+    addKeyBindingBtnListeners();
+}
+
+const refreshKeyBindingBtn = () => {
+    btnKey1.innerText = currentPreferences.keyBinding[1].toUpperCase();
+    btnKey2.innerText = currentPreferences.keyBinding[2].toUpperCase();
+    btnKey3.innerText = currentPreferences.keyBinding[3].toUpperCase();
+    btnKey4.innerText = currentPreferences.keyBinding[4].toUpperCase();
+}
+
+const addKeyBindingBtnListeners = () => {
+    btnKey1.addEventListener("click", onKeyBindingBtn1);
+    btnKey2.addEventListener("click", onKeyBindingBtn2);
+    btnKey3.addEventListener("click", onKeyBindingBtn3);
+    btnKey4.addEventListener("click", onKeyBindingBtn4);
+}
+
+//remove key binding related EventListeners
+const removeKeyBindingListeners = () => {
+    btnKey1.removeEventListener("click", onKeyBindingBtn1);
+    btnKey2.removeEventListener("click", onKeyBindingBtn2);
+    btnKey3.removeEventListener("click", onKeyBindingBtn3);
+    btnKey4.removeEventListener("click", onKeyBindingBtn4);
+    window.removeEventListener("keypress", onKey1Binding);
+    window.removeEventListener("keypress", onKey2Binding);
+    window.removeEventListener("keypress", onKey3Binding);
+    window.removeEventListener("keypress", onKey4Binding);
+}
+
+const onKeyBindingBtn1 = () => {
+    onKeyBindingBtn(1);
+}
+
+const onKeyBindingBtn2 = () => {
+    onKeyBindingBtn(2);
+}
+
+const onKeyBindingBtn3 = () => {
+    onKeyBindingBtn(3);
+}
+
+const onKeyBindingBtn4 = () => {
+    onKeyBindingBtn(4);
+}
+
+const onKeyBindingBtn = (num) => {
+    removeKeyBindingListeners();
+    window.addEventListener("click", onClickOutside);
+    switch(num){
+        case 1:
+            window.addEventListener("keypress", onKey1Binding);
+            break;
+        case 2:
+            window.addEventListener("keypress", onKey2Binding);
+            break;
+        case 3:
+            window.addEventListener("keypress", onKey3Binding);
+            break;
+        case 4:
+            window.addEventListener("keypress", onKey4Binding);
+    }
+    
+}
+
+//cancels key binding
+const onClickOutside = () => {
+    window.removeEventListener("keypress",onKeyBinding);
+    window.removeEventListener("click", onClickOutside);
+    addKeyBindingBtnListeners();
+}
+
+const onKey1Binding = (e) => {
+    onKeyBinding(e, 1);
+}
+
+const onKey2Binding = (e) => {
+    onKeyBinding(e, 2);
+}
+
+const onKey3Binding = (e) => {
+    onKeyBinding(e, 3);
+}
+
+const onKey4Binding = (e) => {
+    onKeyBinding(e, 4);
+}
+
+const onKeyBinding = (e, num) => {
+    window.removeEventListener("click", onClickOutside);
+    removeKeyBindingListeners();
+    let keyValue = e.key;
+    let other = isUsedByOther(num, keyValue);
+    if(other === -1){
+        currentPreferences.keyBinding[num] = keyValue;
+    }else{
+        keySwap(num, other);
+    }
+    refreshKeyBindingBtn();
+    addKeyBindingBtnListeners();
+}
+
+const isUsedByOther = (keyNum, keyValue) => {
+    for(let i=1; i<=4; i++){
+        if(i != keyNum && currentPreferences.keyBinding[i] === keyValue){
+            return i;
+        }
+    }
+    return -1;
+}
+
+const keySwap = (keyNum1, keyNum2) => {
+    if(keyNum1 !== keyNum2){
+        let temp = currentPreferences.keyBinding[keyNum1];
+        currentPreferences.keyBinding[keyNum1] = currentPreferences.keyBinding[keyNum2];
+        currentPreferences.keyBinding[keyNum2] = temp;
+    }
 }
 
 export default OptionsPage;
