b70ff567d0c282a26c7c02be7a6ebeb218422eec
Baptiste Honnay <honnaybaptiste@gmail.com>
2020-12-10 10:13:32 +0100

    add - scrolling with play btn

--- src/Components/PhaserEdit/EditPage.js
+++ src/Components/PhaserEdit/EditPage.js
@@ -28,10 +28,10 @@ let pageHtml = `
         <input id="timeLine" type="range" class="form-control-range" min="0" max="1" step="0.001" value="0"/>
     </div>
     <div class="col-1">
-        <button class="btn btn-secondary">Play/Pause</button>
+        <button id="playBtn" class="btn btn-secondary">Play</button>
     </div>
     <div class="col-1">
-        <p><span id="currentTime">00:00</span>/<span>00:45</span></p>
+        <p><span id="currentTimer">00:00</span>/<span id="endTimer">00:45</span></p>
     </div>
 </div>
 <div class="row mt-5">
@@ -42,24 +42,17 @@ let pageHtml = `
 </div>
 `;
 
-/*
-let ldd = [[0, 0, 3500], [0, 1, 3780], [0, 0, 4100], [0, 1, 4420], //libre de droits ... 
-    [0, 3, 7320], [0, 2, 7630], [0, 1, 7975], [0, 0, 8310], [0, 1, 8640], [0, 2, 8890], [1, 3, 9185, 9975], // générique libre de droiiits ...
-    [0, 0, 10740], [0, 1, 11010], [0, 0, 11300], [0, 1, 11605], // (libre de droits...)
-    [0, 2, 12300], [0, 3, 12300], [0, 0, 12650], [0, 1, 12650], [0, 2, 13065], [0, 3, 13065], [0, 0, 13395], [0, 3, 13395], [0,1,13675], [0,2,13675], [0, 0, 13960], [0, 3, 13960], //cette chanson est à moi ... (double)
-    [0, 3, 15190], [0, 2, 15435], [0, 3, 15790], [0, 2, 16100], [0, 3, 16410], // YouTube l'enlève pas ...
-    [0, 0, 17615], [0, 1, 17890], [0, 0, 18195], [0, 1, 18496], [0, 0, 18738], // c'est libre de doits ...
-    [1, 3, 19550, 20000], [1, 2, 20000, 20600], [1, 1, 20600, 21230], [1, 0, 21230, 22100], // liiiiiibreuuuuuuuh deuuuuuuuuuh droiiiiiiiits ...
-    [0, 2, 22650], [0, 0, 22990], [0, 3, 23255], [0, 1, 23565], // libre de droits ...
-    [0, 0, 26540], [0, 1, 26750], [0, 0, 27135], [0, 1, 27455], [0, 0, 27810], [0, 1, 28095], [1, 0, 28395, 29185], [1, 1, 28395, 29185], //générique libre de droiiits ...
-    [0, 3, 29930], [0, 2, 30200], [0, 1, 30630], [0, 0, 30845],  //(libre de droiiits ...)
-    [0, 0, 31670], [0, 1, 31985], [0, 2, 32280], [0, 3, 32530], [0, 3, 32875], [0, 2, 33210],   //cette chanson est à moi ... (double) 
-    [0, 0, 34385], [0, 1, 34650], [0, 0, 35015], [0, 1, 35300], [0, 0, 35610], //YouTube l'enlèves pas ...
-    [0, 0, 36810], [0, 2, 36810], [0, 1, 37100], [0, 3, 37100], [0, 0, 37420], [0, 2, 37420], [0, 1, 37700], [0, 3, 37700], [1, 0, 38020, 38840], [1, 2, 38020, 38840], // c'est libre de droits.
-    [0, 1, 40665], //Libre ...
-    [0, 1, 41710], [0, 0, 41920] // de droits.
-]
-*/
+let duration;
+let timeLine;
+let playBtn;
+let currentTimer;
+let endTimer;
+let scene;
+let refreshIntervalID;
+let state = 0; //0 = pause; 1 = play
+let currentTime = 0;
+
+const refreshTime = 25; //ms
 
 const EditPage = (data) => {
     /*
@@ -85,13 +78,22 @@ const EditPage = (data) => {
     }
     */
 
+    duration = 45000; //STUB (get from data)
+
     page.innerHTML = pageHtml;
+
+    timeLine = document.querySelector("#timeLine");
+    currentTimer = document.querySelector("#currentTimer");
+    endTimer = document.querySelector("#endTimer");
+    playBtn = document.querySelector("#playBtn");
+
+    initTimer();
 
     const editScreenWidth = 4/5 * window.innerWidth; 
     const editScreenHeight = 1/2 * window.innerHeight;
-    const duration = 45000;
+    
 
-    let scene = new EditScene(editScreenWidth, editScreenHeight, duration)
+    scene = new EditScene(editScreenWidth, editScreenHeight, duration)
 
     let config = {
         type: Phaser.AUTO,
@@ -104,12 +106,8 @@ const EditPage = (data) => {
 
     let game = new Phaser.Game(config);
     
-    let timeLine = document.querySelector("#timeLine");
-    timeLine.addEventListener("change", ()=> {
-        let time = duration * timeLine.value;
-        console.log("update time to "+time);
-        scene.updateCurrentTime(time);
-    })
+    timeLine.addEventListener("change", onTimeChange);
+    playBtn.addEventListener("click", togglePlay);
 
     /*
     let btn = document.querySelector("#publish");
@@ -139,6 +137,94 @@ const publish = (beatmap, user) => {
 
 const onBeatmapPublication = (data) => {
     console.log("Success: res = ", data);
+}
+
+//returns current time in ms
+const getTimeFromTimeline = () => {
+    return duration * timeLine.value;
+}
+
+const setTimelineTo = (time) => {
+    timeLine.value = time/duration;
+    scene.updateCurrentTime(time);
+    setTimer(time);
+}
+
+//converts time in ms to mm:ss format (returns string)
+const convertMsToDisplay = (time) => {
+    let allSeconds = Math.floor(time/1000);
+    let seconds = allSeconds%60;
+    let minutes = Math.floor(allSeconds/60);
+    if(seconds < 10){
+        seconds = "0"+seconds;
+    }
+    if(minutes < 10){
+        minutes = "0"+minutes;
+    }
+    let res = minutes + ":" + seconds;
+    return res;
+}
+
+//sets currentTimer
+const setTimer = (time) => {
+    currentTimer.innerText = convertMsToDisplay(time);
+}
+
+//sets timer to 0 and end time to duration
+const initTimer = () => {
+    currentTimer.innerText = convertMsToDisplay(0);
+    endTimer.innerText = convertMsToDisplay(duration);
+}
+
+const play = () => {
+    if(state === 0){
+        displayPauseBtn();
+        timeLine.disabled = true;
+        refreshIntervalID = setInterval(incrementTime, refreshTime); 
+        state = 1;   
+    }
+}
+
+const pause = () => {
+    if(state === 1){
+        displayPlayBtn();
+        timeLine.disabled = false;
+        clearInterval(refreshIntervalID);
+        state = 0;
+    }
+}
+
+const displayPlayBtn = () => {
+    playBtn.innerText = "Play";
+}
+
+const displayPauseBtn = () => {
+    playBtn.innerText = "Pause";
+}
+
+const togglePlay = () => {
+    switch(state){
+        case 0:
+            play();
+            break;
+        case 1:
+            pause();
+    }
+}
+
+//increments timeline by [refreshTime] ms
+const incrementTime = () => {
+    console.log("time++");
+    currentTime += refreshTime;
+    setTimelineTo(currentTime);
+}
+
+//change time with line
+const onTimeChange = () => {
+    currentTime = getTimeFromTimeline();
+    console.log("update time to "+currentTime);
+    setTimer(currentTime);
+    scene.updateCurrentTime(currentTime);
 }
 
 const onError = (err) => {
--- src/Components/PhaserEdit/EditScene.js
+++ src/Components/PhaserEdit/EditScene.js
@@ -16,7 +16,7 @@ export default class EditScene extends Phaser.Scene {
 
         //time
         this.duration = duration;
-        this.screenTimeSpan = 5000; //ms (time between oposing ends of the screen)
+        this.screenTimeSpan = 2500; //ms (time between oposing ends of the screen)
         this.currentTime = 0; //ms (time at x=0)
 
         //lines
@@ -44,6 +44,8 @@ export default class EditScene extends Phaser.Scene {
         this.drawLines();
 
         this.createSimpleNote(1, 1000);
+        this.createSimpleNote(2, 1500);
+        this.createSimpleNote(3, 2000)
     }
 
     //lines
@@ -70,7 +72,16 @@ export default class EditScene extends Phaser.Scene {
     //changes currentTime and updates display (to be called by EditPage)
     updateCurrentTime(time) {
         this.currentTime = time;
-        //TODO
+        this.updateNotes();
+    }
+
+    updateNotes() {
+        this.beatmap.forEach(noteBundle => {
+            if(noteBundle.note[0] === 0){//single note
+                let time=noteBundle.note[2];
+                noteBundle.sprite.setX(this.getXFromTime(time));
+            }
+        });
     }
 
     //lineNum integer between 0 and 3
