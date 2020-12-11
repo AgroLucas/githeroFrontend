d557d91cf0e415f81512bc296218dce8dffd4e59
AgroLucas <lucasagro1122@gmail.com>
2020-12-11 23:49:25 +0100

    made Navbar responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -11,28 +11,36 @@ const Navbar = () => {
     if(user){ // connecté
         navbarHtml = `
             <nav class="navbar navbar-expand-sm navbar-dark" id="navbar">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <img id="whiteLogo" src="` + whiteLogo + `" alt="whiteLogo" href="#" data-uri="/">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><button type="button" class="btn btn-danger active" href="#" data-uri="/list">Jouer</button></li>
-                    <li class="nav-item"><button type="button" class="btn btn-secondary active" href="#" data-uri="/edit">Editer</button></li>
-                    
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><button type="button" class="btn btn-secondary active" href="#" data-uri="/logout">Déconnexion</button></li>
-                    <li class="nav-item"><img id="profile" src="` + profile + `" alt="profil" href="#" data-uri="/login"/></li>
-                </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-danger active" href="#" data-uri="/list">Jouer</button></li>
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-secondary active" href="#" data-uri="/edit">Editer</button></li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-secondary active" href="#" data-uri="/logout">Déconnexion</button></li>
+                    </ul>
+                </div>
             </nav>`;
     } else { // pas connecté
         navbarHtml = `
             <nav class="navbar navbar-expand-sm bg-primary navbar-dark" id="navbar">
-                <img id="whiteLogo" src="` + whiteLogo + `" alt="whiteLogo" href="#" data-uri="/">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><button type="button" class="btn btn-danger active" href="#" data-uri="/list">Jouer</button></li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item"><button type="button" class="btn btn-secondary active" href="#" data-uri="/login">Se connecter</button></li>
-                    <li class="nav-item"><button type="button" class="btn btn-secondary active" href="#" data-uri="/register">S'inscrire</button></li>
-                </ul>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <img id="whiteLogo" src="` + whiteLogo + `" alt="whiteLogo" href="#" data-uri="/"/>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-danger active" href="#" data-uri="/list">Jouer</button></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-secondary active" href="#" data-uri="/login">Se connecter</button></li>
+                        <li class="nav-item mt-2 mb-2"><button type="button" class="btn btn-secondary active" href="#" data-uri="/register">S'inscrire</button></li>
+                    </ul>
+                </div>
             </nav>`;
     }
     navBar.innerHTML = navbarHtml;
