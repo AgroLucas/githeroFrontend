4edfd4674124319a8f9445bd53fbaa4075ff251e
AgroLucas <lucasagro1122@gmail.com>
2020-12-12 15:52:29 +0100

    change weird ajax foreach into every in rankingPage

--- src/Components/RankingPage.js
+++ src/Components/RankingPage.js
@@ -16,20 +16,23 @@ const loadScoreboard = async () => {
     }).then( response =>{
         let pageHtml = `
 <div class="side_page ranking container d-flex h-100">
-    <table class="table table-dark" style="text-align: center;">
+    <table class="table table-dark text-center">
         <thead>
             <tr><th colspan="3"><h5>Meilleurs Scores Généraux</h5></th></tr>
         </thead>
         <tbody>`
         let index = 1;
-        for(const ELEMENT of response.totalscore){
-           pageHtml += `
+        response.totalscore.every(element => {
+            if (index==50)
+                return false;
+            pageHtml += `
             <tr>
                 <td>` + index++ +`</td>
-                <td>`+ ELEMENT.username +`</td>
-                <td>` + ELEMENT.totalHighscore + `</td>
-            </tr>`
-        }
+                <td>`+ element.username +`</td>
+                <td>` + element.totalHighscore + `</td>
+            </tr>`;
+            return true;
+        });
         `</tbody>
     </table>
 </div>`;
@@ -37,42 +40,6 @@ const loadScoreboard = async () => {
     });
 
 }
-
-let pageHtml = `
-<div class="side_page ranking container d-flex h-100">
-    <table class="table table-dark" style="text-align: center;">
-        <thead>
-            <tr><th colspan="3"><h5>Meilleurs Scores Généraux</h5></th></tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Pseudo</td>
-                <td>Score</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Pseudo</td>
-                <td>Score</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Pseudo</td>
-                <td>Score</td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Pseudo</td>
-                <td>Score</td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Pseudo</td>
-                <td>Score</td>
-            </tr>
-        </tbody>
-    </table>
-</div>`;
 
 
 
