85dd73058a47202ac690404ed69e26ee29513e03
AgroLucas <lucasagro1122@gmail.com>
2020-12-12 14:13:55 +0100

    change name:email position

--- src/Components/AboutUsPage.js
+++ src/Components/AboutUsPage.js
@@ -4,10 +4,10 @@ let pageHtml = `
     <p>Nous sommes étudiants en 2ème année en Informatique de gestion à L'institut Paul Lambin.</p>
     <p>Nous avons dû créer ce site dans le cadre de notre cours de Javascript.</p>
     <p>Si vous avez des questions à propos du site en général ou du jeu, veuillez contacter l'un de nous par mail :</p>
-    <p>Lucas Agrò : lucas.y.agro@student.vinci.be</p>
-    <p>Anis Assaidi : anis.assaidi@student.vinci.be</p>
-    <p>Bastien Boutte : bastien.boutte@student.vinci.be</p>
-    <p>Baptiste Honnay : baptiste.honnay@student.vinci.be</p>    
+    <p>Lucas Agrò :<br/> lucas.y.agro@student.vinci.be</p>
+    <p>Anis Assaidi :<br/> anis.assaidi@student.vinci.be</p>
+    <p>Bastien Boutte :<br/> bastien.boutte@student.vinci.be</p>
+    <p>Baptiste Honnay :<br/> baptiste.honnay@student.vinci.be</p>    
     <p class ="mt-5">Note : notre logo est dérivé du logo de GIT de Jason Long.</p>
 
 </div>`;
